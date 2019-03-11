8a93c6daaf51834e1ab623aee6737d0517a82161
Andrei Popovici <andrei.popovici@sainsburys.co.uk>
2019-03-11 18:01:00 +0000

    Added schema for GraphQL

--- users/server.js
+++ users/server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const expressGraphQL = require('express-graphql');
+const schema = require('./schema/schema')
 
 const app = express();
 app.use('/graphql', expressGraphQL({
+  schema,
   graphiql: true // param only for developemnt env   
 }))
 
 
-const PORT  = 4000;
+const PORT = 4000;
 app.listen(PORT, () => {
-    console.log('Listening on:' + PORT);
+  console.log('Listening on:' + PORT);
 })
